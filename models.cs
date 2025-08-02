d85ed3d8a51b60ccf30fd4cf152f837f0c18a1a7
r41ngee <r41ngee@yandex.ru>
2025-08-02 04:29:08 +0300

    Добавлен файл models.cs с определениями классов Person и Relationship, а также перечисления Gender и RelationshipType.

---
+++ models.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace FG.Models;
+
+public enum Gender {
+    Male,
+    Female,
+}
+
+public class Person {
+    public Guid id = Guid.NewGuid();
+    public string name { get; private set; }
+    public Gender gender { get; private set; }
+    public DateOnly? birthDate { get; private set; }
+    public DateOnly? deathDate { get; private set; }
+
+    public Person(
+        string name,
+        Gender gender,
+        DateOnly? birthDate,
+        DateOnly? deathDate
+    ) {
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.deathDate = deathDate;
+    }
+}
+
+public class Relationship {
+    public Guid fromId;
+    public Guid toId;
+    public RelationshipType type;
+    public DateOnly? startDate;
+    public DateOnly? endDate;
+}
+
+public enum RelationshipType {
+    Parent,
+    Child,
+    Spouse,
+    FullSibling,
+    HalfSibling,
+}
