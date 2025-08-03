e150d2cdf751179eb8e3f5f7a4deb4907de1608f
r41ngee <r41ngee@yandex.ru>
2025-08-03 23:31:55 +0300

    Изменена структура класса Tree: заменен список relations на словарь, переименовано свойство Version в version, удалены методы добавления и удаления людей и отношений.

--- tree.cs
+++ tree.cs
@@ -5,34 +5,9 @@ using System.Collections.Generic;
 
 public class Tree {
     Dictionary<Guid, Person> people { get; } = [];
-    List<Relation> relations { get; } = [];
+    Dictionary<Guid, Relation> relations { get; } = [];
 
-    public ulong Version { get; private set; } = 0;
+    public ulong version {get; private set;}
 
-    public bool addPerson(Person person) {
-        bool validation = !people.ContainsKey(person.id);
-        if (validation) {
-            people.Add(person.id, person);
-            Version++;
-        }
-        return validation;
-    }
-
-    public bool removePerson(Person person) {
-        bool validation = people.ContainsKey(person.id);
-        if (validation) {
-            people.Remove(person.id);
-            Version++;
-        }
-        return validation;
-    }
-
-    public bool addRelationship(Relation relation) {
-        bool validation = !people.ContainsKey(relation.fromId) || !people.ContainsKey(relation.toId);
-        if (validation) {
-            relations.Add(relation);
-            Version++;
-        }
-        return false;
-    }
+    
 }
